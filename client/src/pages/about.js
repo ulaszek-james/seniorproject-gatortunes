f7c332044d78511cf40c368be8aff2c3561598d6
James Ulaszek <jaminthoseconcerts@gmail.com>
2022-12-01 14:50:35 -0500

    about page styling

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -4,13 +4,19 @@ import TestComponent from "../TestComponent";
 
 const About = () => {
   return (
-    <div>
-      <h1>This is the about page</h1>
-      <h2>Our mission statement</h2>
-      <h2>Meet the team</h2>
-    </div>
+    <>
+    <section className="stars-out">
+      <div className="home-page-container">
+        <h1>This is the about page</h1>
+        <h2>Our mission statement</h2>
+        <h2>Meet the team</h2>
+      </div>
+    </section>
+    </>
   );
 };
+// Sofie here I want the svg file serving as the background img for entire page (look at .stars-out class)
+// It's only spanning the height of the container rn
 
 export default About;
 //<TestComponent />
