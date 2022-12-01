1606657c8d8babb82fdad7caa616ddaee722db9a
James Ulaszek <jaminthoseconcerts@gmail.com>
2022-12-01 18:58:42 -0500

    updated home page to no longer display login btn

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container } from "react-bootstrap";
+import { UserContext } from "../contexts/googleuser.context";
 import GetProfileData from "../GetProfileData";
 import '../styles.css';
 
@@ -7,12 +8,21 @@ function login() {
   window.location = '../login';
 }
 const Home = () => {
+  const { currentUser } = useContext(UserContext);
   return (
     <>
       <div className="spacer layered-waves"></div>
       <div className="home-page-container">
         <div className="home-page-title">Gator Tunes</div>
-        <button className="home-page-btn" onClick={login}> Login </button>
+        {!currentUser ? (
+          <div>
+            <button className="home-page-btn" onClick={login}> Login </button>
+          </div>
+        ) : (
+          <div>
+            <p>Already logged in :D</p>
+          </div>
+        )}
       </div>
     </>
   );
